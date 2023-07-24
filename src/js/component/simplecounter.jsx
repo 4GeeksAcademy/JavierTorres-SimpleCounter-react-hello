8cb0b5765c9ad63b2c621aa61320c3677c3b3f43
Javier Torres <127144225+JavierOTorres@users.noreply.github.com>
2023-07-24 23:33:00 +0000

    Done

---
+++ src/js/component/simplecounter.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+const Simplecounter = () => {
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount(count + 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    })
+
+    const numDivBootstrap = "col ms-2 py-4 border-start border-end border-3 border-dark rounded bg-dark bg-opacity-50";
+
+    const sixDigits = (n) => {
+        let arr = n.toString().split('');
+        while (arr.length < 6) arr.unshift('0');
+        return arr.map((n)=> {
+            return (
+                <div className={numDivBootstrap} >
+                    <h1 className='text-white m-0'>{n}</h1>
+                </div>
+            );
+        });
+    }
+    
+    return (
+        <>
+            <div className={`${numDivBootstrap} d-flex justify-content-center align-items-center`}>
+                <i className="fa-solid fa-stopwatch text-white display-6"></i>
+            </div>
+            {sixDigits(count)}
+        </>
+    )
+}
+
+export default Simplecounter 
